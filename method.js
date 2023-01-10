58bda63f08739c493f889fab2e412d9f1fb02e60
lby132 <quddbscjswow@naver.com>
2023-01-10 23:46:19 +0900

    test

---
+++ method.js
@@ -0,0 +1,31 @@
+// const apple = {
+//     name: 'apple',
+//     display: function() {
+//         console.log(`${this.name}: 사과`);
+//     }
+// }
+
+// apple.display();
+// console.log(apple.name);
+
+// const orange = {
+//     name: 'orange',
+//     display: function() {
+//         console.log(`${this.name}: 오렌지`);
+//     }
+// }
+
+//생성자 함수
+function Fruit(name, emoji) {
+    this.name = name;
+    this.emoji = emoji;
+    this.display = () => {
+        console.log(`${this.name}: ${this.emoji}`);
+    }
+}
+
+const apple = new Fruit('apple', '사과');
+const orange = new Fruit('orange', '오렌지');
+
+console.log(apple);
+console.log(orange);
---
+++ object/basic.js
@@ -0,0 +1,24 @@
+//클래스
+class Fruit {
+    constructor(name, emoji) {
+        this.name = name;
+        this.emoji = emoji;
+    }
+
+    display = () => {
+        console.log(`${this.name}: ${this.emoji}`);
+    };
+
+}
+
+const apple = new Fruit('apple', '사과');
+const orange = new Fruit('orange', '오렌지');
+
+console.log(apple);
+console.log(orange);
+console.log(apple.name);
+console.log(apple.emoji);
+apple.display();
+
+// obj는 객체이고, 그 어떤 클래스의 인스턴스도 아니다.
+const obj = { name: 'ellie' };
